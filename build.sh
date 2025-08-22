831ea26b6879179371bc1d4d5cd65e8d9108bd4f
tianlan <1797709323@qq.com>
2025-08-22 22:13:52 +0800

    Support bazel offline build

--- build.sh
+++ build.sh
@@ -6,22 +6,22 @@ readonly current_script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/n
 readonly install_dir="$(pwd)/output"
 
 clean() {
-    rm -rf $install_dir
+        rm -rf $install_dir
 }
 
 build() {
-    bazel build $build_config main
-    bazel build $build_config common
-    bazel build $build_config //src:node
+        # bazel build main
+        # bazel build common
+        bazel build //src:node
 }
 
 test() {
-    bazel test $build_config --test_output=all //src/common/math:vec2d_test
-    bazel test $build_config --test_output=all //test/...
+        bazel test --test_output=all //src/common/math:vec2d_test
+        bazel test --test_output=all //test/...
 }
 
 install() {
-    bazel run $build_config install -- $install_dir
+        bazel run install -- $install_dir
 }
 
 refresh() {
@@ -36,72 +36,72 @@ refresh() {
 }
 
 format_bazel() {
-    [ $(command -v buildifier-linux-amd64) ] || {
-        printf $(
-            tput setaf 3
-            tput bold
-        )'buildifier-linux-amd64 not found, please install it first\n'$(tput sgr0)
-        return
-    }
-    find src -type f \( \
-        -name "BUILD" \
-        -o -name "*.bzl" \
-        -o -name "WORKSPACE" \
-        -o -name "BUILD.bazel" \
-        -o -name "*.BUILD" \
-        \) -exec "buildifier-linux-amd64" {} \;
+        [ $(command -v buildifier-linux-amd64) ] || {
+                printf $(
+                        tput setaf 3
+                        tput bold
+                )'buildifier-linux-amd64 not found, please install it first\n'$(tput sgr0)
+                return
+        }
+        find src -type f \( \
+                -name "BUILD" \
+                -o -name "*.bzl" \
+                -o -name "WORKSPACE" \
+                -o -name "BUILD.bazel" \
+                -o -name "*.BUILD" \
+                \) -exec "buildifier-linux-amd64" {} \;
 }
 
 cmake_build() {
-    rm -rf $current_script_dir/output $current_script_dir/build
-    cmake -S $current_script_dir/cmake -B $current_script_dir/build \
-        -G Ninja \
-        -DCMAKE_BUILD_TYPE=Debug \
-        -DCMAKE_CXX_FLAGS=-fdiagnostics-color=always \
-        -DCMAKE_INSTALL_PREFIX=output \
-        -DCMAKE_TOOLCHAIN_FILE=rpi3-toolchain.cmake
-    # -DENABLE_ASAN=ON
-    ninja -C build -j 8
-    ninja -C build install
+        rm -rf $current_script_dir/output $current_script_dir/build
+        cmake -S $current_script_dir/cmake -B $current_script_dir/build \
+                -G Ninja \
+                -DCMAKE_BUILD_TYPE=Debug \
+                -DCMAKE_CXX_FLAGS=-fdiagnostics-color=always \
+                -DCMAKE_INSTALL_PREFIX=output \
+                -DCMAKE_TOOLCHAIN_FILE=rpi3-toolchain.cmake
+        # -DENABLE_ASAN=ON
+        ninja -C build -j 8
+        ninja -C build install
 }
 
 bazel_build() {
-    clean
-    build
-    # test
-    install
-    refresh
-    format_bazel
-    # bazel shutdown
+        clean
+        build
+        # test
+        install
+        refresh
+        format_bazel
+        # bazel shutdown
 }
 
 case $1 in
 clean)
-    clean
-    bazel clean --expunge
-    ;;
+        clean
+        bazel clean --expunge
+        ;;
 rpi)
-    build_config=(
-        --platforms=//:rpi
-        # --config asan
-    )
-    bazel_build
-    ;;
+        build_config=(
+                --platforms=//:rpi
+                # --config asan
+        )
+        bazel_build
+        ;;
 x86)
-    build_config=(
-        # --config asan
-    )
-    bazel_build
-    ;;
+        build_config=(
+                # --config asan
+        )
+        bazel_build
+        ;;
 *)
-    build_config=(
-        # --config asan
-    )
-    bazel_build
-    ;;
+        build_config=(
+                # --config asan
+        )
+        bazel_build
+        ;;
 esac
 
 printf $(
-    tput setaf 2
-    tput bold
+        tput setaf 2
+        tput bold
 )'Build success!\n'$(tput sgr0)
---
+++ pch/main.cc
@@ -0,0 +1,6 @@
+#include "log.h"
+
+int main(int argc, char **argv) {
+  std::string s = "Hi";
+  return 0;
+}
