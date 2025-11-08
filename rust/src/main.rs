596bbf369d75f69b3ab6b93ba3d14c4784f20ffb
tianlan.wang <1797709323@qq.com>
2025-11-08 23:06:04 +0800

    Update rust demo

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
     println!("Hello, world!");
+    
+    let mut count = 1;
+    loop {
+        println!("This loop will run forever.");
+        count = count + 1;
+        if count > 5 {
+            break;
+        }
+    }
 }
